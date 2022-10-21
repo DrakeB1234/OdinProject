59deeae238cac1b74a376b56f7e70a351dbc9331
Drake Buentello <dbuentello211607@gmail.com>
2022-10-21 09:38:43 -0500

    finshed book constructor

--- JSCourse/exercises/constructor.js
+++ JSCourse/exercises/constructor.js
@@ -1,21 +1,21 @@
-function Book (title, author, pages, read){
-    this.title = title,
-    this. author = author,
-    this.pages = pages,
-    this.read = read,
+function Book(title, author, pages, read){
+    this.title = title
+    this. author = author
+    this.pages = pages
+    this.read = read
 
     this.info = function() {
-        info = (title, " by ", author, ", ", pages);
+        let info = (title + " by " + author + ", " + pages + " pages, ");
         if (read == true) {
             info += "already read";
         }
         else {
             info += "not read yet";
         }
-        return (title, author, pages, read);
+        return info;
     }
 }
 
 const TheHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
 
-TheHobbit.info();
+console.log(TheHobbit.info());
