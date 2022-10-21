521cabb854955fe34fef0b62e32f18498a034c0d
Drake Buentello <dbuentello211607@gmail.com>
2022-10-21 10:50:41 -0500

    work on prototypes

--- JSCourse/exercises/prototypalinheritance.js
+++ JSCourse/exercises/prototypalinheritance.js
@@ -1,6 +1,17 @@
 function apple(color, weight) {
-    this.color = color
-    this.weight = weight
+    this.color = color;
+    this.weight = weight;
 }
 
-apple.prototype
+apple.prototype = {
+    eat(){ return "you eat the " + this.color + " colored apple" },
+    throw(){ return "you throw the " + this.color + " colored apple" }
+}
+
+// all of these objects inherit the prototype
+const apple1 = new apple("red", 23);
+const apple2 = new apple("green", 12);
+const apple3 = new apple("purple", 54);
+
+console.log(apple1.eat());
+console.log(apple3.throw());
