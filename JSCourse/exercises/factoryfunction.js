402df057d330c26e71a34aaa27d823d6c2b3819c
Drake Buentello <dbuentello211607@gmail.com>
2022-10-24 14:21:54 -0500

    creating objects directly inline

--- JSCourse/exercises/factoryfunction.js
+++ JSCourse/exercises/factoryfunction.js
@@ -8,3 +8,11 @@ const jeff = personFactory('jeff', 27);
 console.log(jeff);
 
 jeff.sayHello();
+
+// can create objects directly inline
+const firstName = 'Joe';
+const color = 'red';
+const number = 23;
+const food = 'rice';
+
+console.log({ firstName, color, number, food }); // creates object directly in console log statement
