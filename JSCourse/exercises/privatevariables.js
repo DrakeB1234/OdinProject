f462e0b0644678fc1038ce1daaff1c1957d123e3
Drake Buentello <dbuentello211607@gmail.com>
2022-10-27 09:00:09 -0500

    returning objects with factory function

--- JSCourse/exercises/privatevariables.js
+++ JSCourse/exercises/privatevariables.js
@@ -1,24 +1,38 @@
-const counterCreator = () => {
-    let count = 0;
-    // return () => {
-    //   console.log(count);
-    //   count++;
-    // };
-    const counter = () => {
-        print();
-        count++;
-    }
+// const counterCreator = () => {
+//     let count = 0;
+//     // return () => {
+//     //   console.log(count);
+//     //   count++;
+//     // };
+//     const counter = () => {
+//         print();
+//         count++;
+//     }
 
-    const print = () => {
-        console.log(count);
-    }
+//     const print = () => {
+//         console.log(count);
+//     }
 
-    return counter;
-  };
+//     return counter;
+//   };
   
-  const counter = counterCreator();
+//   const counter = counterCreator();
   
-  counter(); 
-  counter(); 
-  counter(); 
-  counter();
+//   counter(); 
+//   counter(); 
+//   counter(); 
+//   counter();
+
+const factoryFunction = (name, age) => {
+    return { name, age };
+};
+
+let name = [];
+
+name.push(factoryFunction('joey', 12));
+name.push(factoryFunction('mark', 34));
+name.push(factoryFunction('hailey', 122));
+
+name.forEach((e) => {
+    console.log(e);
+});
