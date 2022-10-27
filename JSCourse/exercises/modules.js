0a7b7848e44bfaeaec7b3975195007c1bef614c8
Drake Buentello <dbuentello211607@gmail.com>
2022-10-27 07:44:41 -0500

    asdf

--- JSCourse/exercises/modules.js
+++ JSCourse/exercises/modules.js
@@ -1,10 +1,36 @@
-// myModule.sayName();
+const changer = () => {
+    let colors = ['red', 'green', 'purple'];
 
-// const people = {
-//     people: [],
-//     template: $('#data-get').html(),
-//     init: function () {
-//         tj
-//     }
-    
-// }
+    const addColor = (x) => {
+        colors.push(x);
+  
+        console.log(`Added color ${colors[3]}`);
+        console.log(colors);
+    }
+
+    const changeColor = () => {
+        console.log("changing color");
+        // get random number
+        const rand = Math.floor(Math.random() * colors.length);
+        console.log(colors);
+        document.getElementById('data-get').style.backgroundColor = colors[rand];
+    }
+    // returning functions
+    return { addColor, changeColor };
+}
+
+// event for change color button
+document.getElementById('change-button').addEventListener('click', () => {
+    const colorHandler = changer();
+
+    colorHandler.changeColor();
+});
+
+// event for add color button
+document.getElementById('add-button').addEventListener('click', () => {
+    const colorHandler = changer();
+
+    const color = document.getElementById('add-color').value;
+
+    colorHandler.addColor(color);
+});
