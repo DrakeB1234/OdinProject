24c9babd82cf140fe74debbf8bdb5f93d60c5d0e
Drake Buentello <dbuentello211607@gmail.com>
2022-10-27 11:54:53 -0500

    added basic functionality

--- JSCourse/assignments/tictactoeproject/index.js
+++ JSCourse/assignments/tictactoeproject/index.js
@@ -7,6 +7,44 @@ const Game = () => {
         [0,0,0]
     ];
 
+    const start = () => {
+        let player = 2;
+        const inputs = document.querySelectorAll('.input-btn-container #input');
+        const player1element = document.getElementById('player1-input');
+        const player2element = document.getElementById('player2-input');
+        player1element.classList.add('active-player');
+
+        inputs.forEach(e => {
+            e.addEventListener('click', (e) => {
+                const value = e.explicitOriginalTarget.value;
+                if (player == 1) {
+                    // ensures that the spot isn't chosen already
+                    if (!(move(value[0], value[1], value[2]))) {
+                        player = 1;
+                    }
+                    else {
+                        player1element.classList.add('active-player');
+                        player2element.classList.remove('active-player');
+                        e.explicitOriginalTarget.disabled;
+                        player = 2;   
+                    }
+                }
+                else {
+                    // ensures that the spot isn't chosen already
+                    if (!(move(value[0], value[1], value[2]))) {
+                        player = 2;
+                    }
+                    else {
+                        player1element.classList.remove('active-player');
+                        player2element.classList.add('active-player');
+                        e.explicitOriginalTarget.disabled = true;
+                        player = 1;   
+                    }
+                }
+            });
+        });
+    };
+
     const move = (player, x, y) => {
         // sets character to player
         if (player == 1){
@@ -20,13 +58,13 @@ const Game = () => {
         if (board[x][y] == 0) {
             board[x][y] = player;
             console.log(`player '${player}' moves to ${x}, ${y}!`);
+            update();
+            return true;
         }
         else {
             console.log('This spot is already chosen!');
+            return false;
         }
-
-        update();
-        return;
     }
 
     const update = () => {
@@ -48,7 +86,12 @@ const Game = () => {
         document.getElementById('data-get').innerHTML = html;
     };
 
-    return { move };
+    return { start };
 };
 
-const newGame = Game();
+document.getElementById('start-game').addEventListener('click', () => {
+    document.getElementById('intro').classList.add('hide');
+    document.getElementById('main').classList.remove('hide');
+    const newGame = Game();
+    newGame.start();
+});
