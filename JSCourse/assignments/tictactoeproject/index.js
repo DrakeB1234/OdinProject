8b45d86b809a4dfbfe26f010c1176511d1143b3b
Drake Buentello <dbuentello211607@gmail.com>
2022-10-31 12:23:13 -0500

    HAVE TO CONTINUE LOGIC SOMETIME TICTACTOE

--- JSCourse/assignments/tictactoeproject/index.js
+++ JSCourse/assignments/tictactoeproject/index.js
@@ -14,46 +14,64 @@ const Game = () => {
     let curPlayer = player1.id;
 
     const gameFunction = (e) => {
+        const playerDis = document.getElementById('player-turn');
         const target = e.explicitOriginalTarget;
-        console.log(e);
         if (curPlayer == 1){
             // changes html
             target.innerHTML = player1.marker;
             // adds to array
-            setPlayer();
+            setPlayer(player1);
+            playerDis.innerHTML = "<h1>Player 2 Turn!</h1>";
             curPlayer = player2.id;
         }
         else {
             // changes html
             target.innerHTML = player2.marker;
             // adds to array
-            setPlayer();
+            setPlayer(player2);
+            playerDis.innerHTML = "<h1>Player 1 Turn!</h1>";
             curPlayer = player1.id;
         }
     };
 
-    const setPlayer = () => {
+    const setPlayer = (player) => {
         // if game is not won
-        const check = checkWin();
-        if (!(check)) {
-            let i = 0;
-            document.querySelectorAll('[input]').forEach(e => {
-                board[i] = e.innerHTML;
-                i++;
-            });
-            console.log(board);
+        let i = 0;
+        document.querySelectorAll('[input]').forEach(e => {
+            board[i] = e.innerHTML;
+            i++;
+        });
+        checkWin(player);
+    };
+
+    const checkWin = (player) => {
+        let j = 0;
+        let match = false;
+        for (let i = 0; i < board.length; i+=3){   
+            if (match != false){
+                console.log(`Player ${player.id} Won!`);
+                endGame();
+                return;
+            }
+            let cur = board[i];
+            for (let j = i; j < (i + 3); j++){
+                if (board[j] != cur){
+                    console.log("J: " + board[j] + " cur: " + cur);
+                    match = false;
+                    continue;
+                }
+                else {
+                    match = true;
+                }
+            }
         }
     };
 
-    const checkWin = () => {
-        let cur = "";
-        // check for row
-        for (let i = 0; i < 3; i++){
-            cur = board[i];
-            if (board[i + 1] != cur) {
-                alert("no match");
-            }
-        }
+    const endGame = () => {
+        // removes event listeners if game won
+        document.querySelectorAll('[input]').forEach(e => {
+            e.removeEventListener('click', gameFunction, { once:true });
+        });  
     };
 
     // sets event listeners
